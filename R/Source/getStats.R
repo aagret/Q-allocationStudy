449d216a16e4209e18a308307c28ba26db533b11
aagret <aagret@petray.ch>
2018-04-11 16:05:30 +0200

    - remove use of external Fx and Rates file now embeded in "main file" + change data names - adapt riskfree frequency (260days per year) - use exofcash&fees datas in port simulation

--- R/Source/getStats.R
+++ R/Source/getStats.R
@@ -4,33 +4,30 @@ getStats <- function(db= database, per= period) {
     
     db <- lapply(db, function(x) lapply(x, function(y) y[per,]))
     
-        if (is.null(db$indexes$returns$EUR)) {
-        
-        per <- index(db)
-        rates <- as.xts(getData("rates.csv"))
-        db <- cbind(db$indexes, rates[per])
+        # if (is.null(db$indexes$returns$EUR)) {
+        # 
+        # per <- index(db)
+        # rates <- as.xts(getData("rates.csv"))
+        # db <- cbind(db$indexes, rates[per])
+        # 
+        # }
     
-        }
-    
-
     ndays <- last(index(db$indexes$returns)) - first(index(db$indexes$returns))
     
-    riskFree <- mean(db$indexes$returns$EUR) / ifelse(ndays <= 365, ndays * 100, 36500)
+    riskFree <- mean(db$indexes$returns$EU1m) / 26000
     
-    db$indexes$returns$EUR <- db$indexes$returns$USD <- NULL
+    db$indexes$returns$US1m <- db$indexes$returns$EU1m <- NULL
     
     db$indexes$cor     <- round(cor(db$indexes$returns), 4)
-    
-    
-    rollCor <- runCor(db$indexes$returns$DEQ, db$indexes$returns$Q_hdg, 30)
+
+    rollCor <- runCor(db$indexes$returns$DEQexa, db$indexes$returns$Q_hdg, 30)
     rollCor[is.na(rollCor)] <- 0
     
     db$indexes$rollCor <- rollCor
-    
+
     stats <- list()
     stats <- lapply(db, calcStats, riskFree)
-    
 
     return(stats)
-    
+
 }
